627a4f232c194829d66b613778efb104b30d7665
LisandroDella <ldella@fi.uba.ar>
2024-07-24 14:18:31 -0300

    Added edit_user and get_by_name with their respective route(put and get) in app.py

--- Backend/app.py
+++ Backend/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify
 from models import db, User, Trophy, Upgrade
+from sqlalchemy.exc import IntegrityError
+import datetime
 
 app = Flask(__name__)
 port = 5000
@@ -49,14 +51,56 @@ def nuevo_usuario():
         #lastclick = data.get('lastclick', None)  # Se establece que el usuario nuevo no tiene click
        
         # Si lastclick no se proporciona, se usará el valor por defecto en el modelo
-        nuevo_usuario = User(name=nuevo_nombre, total_points=total_points, spent_points=spent_points) #removido el last_click hasta resolver lo del horario
-        db.session.add(nuevo_usuario)
+        nuevo_usero = User(name=nuevo_nombre, total_points=total_points, spent_points=spent_points) #removido el last_click hasta resolver lo del horario
+        db.session.add(nuevo_usero)
         db.session.commit()
-        return jsonify({'usuario': {'id': nuevo_usuario.id, 'name': nuevo_usuario.name, 'total_points': nuevo_usuario.total_points, 'spent_points': nuevo_usuario.spent_points}}), 201
+        return jsonify({'usuario': {'id': nuevo_usero.id, 'name': nuevo_usero.name, 'total_points': nuevo_usero.total_points, 'spent_points': nuevo_usero.spent_points}}), 201
     except Exception as error:
         print(error)
         return jsonify({"mensaje": "No se pudo crear el usuario."}), 500 # Esto no deberia pasar nunca
     
+@app.route("/usuarios/", methods=["PUT"])
+def edit_user(user_id):
+    try:
+        usuario = User.query.get('name')
+        if usuario:
+            data=request.json
+            if 'name' in data:
+                usuario.name= data['name']
+            if 'total_points' in data:
+                usuario.total_points = data['total_points']
+            if 'spent_points' in data:
+                usuario.spent_points = data['spent_points']
+            usuario.last_click = datetime.datetime.now()
+
+            db.session.commit()
+            return jsonify({"Mensaje": "Usuario actualizado"}), 200
+        else:
+            return jsonify({"Mensaje": "usuario no existe"}), 404
+    except Exception as error:
+        print(error)
+        return jsonify({"mensaje": "Error al actualizar el usuario"}), 500
+
+
+
+@app.route("/usuarios/<username>", methods=["GET"])
+def get_by_name(username):
+    try:
+        user = User.query.filter_by(name=username).first()
+        if user:
+            user_data = {
+                'id': user.id,
+                'name': user.name,
+                'total_points': user.total_points,
+                'spent_points': user.spent_points,
+                'last_click': user.last_click
+            }
+            return jsonify(user_data)
+        else:
+            return jsonify({"mensaje": "usuario no encontrado."}), 404
+    except Exception as error:
+        print(error)
+        return jsonify({"mensaje": "Error al obtener el usuario."}), 500
 
 
 if __name__ == '__main__':
