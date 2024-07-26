59d0ebb39faea9e68a0aed42e81e5893fe40115a
IGacitua <ezmagacitua@gmail.com>
2024-07-26 17:41:34 -0300

    Added upgrades.sql This allows for easier upgrade table info adding

---
+++ upgrades.sql
@@ -0,0 +1,48 @@
+INSERT INTO upgrades (
+    internal_name, effect, value_tier_1, name_tier_1, value_tier_2, name_tier_2, value_tier_3, name_tier_3, value_tier_4, name_tier_4, value_tier_5, name_tier_5
+    ) 
+VALUES (
+    'pickaxe', 'ADD', 5, 'Iron Pickaxe', 10, 'Bronze Pickaxe', 15, 'Steel Pickaxe', 20, 'Mithril Pickaxe', 30, 'Qubit Pickaxe'
+);
+
+INSERT INTO upgrades (
+    internal_name, effect, value_tier_1, name_tier_1, value_tier_2, name_tier_2, value_tier_3, name_tier_3
+    ) 
+VALUES (
+    'lantern', 'MULTIPLY', 1.2, 'Gas Lamp', 1.5, 'Electric Lantern', 2, 'Flashlight'
+);
+
+INSERT INTO upgrades (
+    internal_name, effect, value_tier_1, name_tier_1, value_tier_2, name_tier_2, value_tier_3, name_tier_3
+    ) 
+VALUES (
+    'assistant', 5, 'Elves', 10, 'Gnomes', 20, 'Dwarves'
+);
+
+INSERT INTO upgrades (
+    internal_name, effect, value_tier_1, name_tier_1
+    ) 
+VALUES (
+    'meals', 'EXPONENT', 1.1, 'Regular Meals'
+);
+
+INSERT INTO upgrades (
+    internal_name, effect, value_tier_1, name_tier_1
+    ) 
+VALUES (
+    'housing', 'EXPONENT', 1.1, 'Good Housing'
+);
+
+INSERT INTO upgrades (
+    internal_name, effect, value_tier_1, name_tier_1
+    ) 
+VALUES (
+    'helmet', 'MULTIPLY', 1.5, 'Mining Helmet'
+);
+
+INSERT INTO upgrades (
+    internal_name, effect, value_tier_1, name_tier_1
+    ) 
+VALUES (
+    'cartographer', 'MULTIPLY', 3, 'Cartographer Assistant'
+);
