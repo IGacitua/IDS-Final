8a47d0e337b4bb26cf7af7d3f62d3ccf048407f7
Tomy <tgomezm@fi.uba.ar>
2024-07-27 02:09:19 -0300

    Optimization with function to make it more readble

--- Backend/app.py
+++ Backend/app.py
@@ -24,43 +24,53 @@ def home():
 # Listar todos los usuarios
 @app.route("/users/", methods=["GET", "POST"])
 def all_users():
-
     if request.method == "GET":
-        try:
-            usuarios = User.query.all()
-            usuarios_data = []
-            for usuario in usuarios:
-                usuario_data = {
-                    'id': usuario.id,
-                    'name': usuario.name,
-                    'total_points': usuario.total_points,
-                    'spent_points': usuario.spent_points,
-                    'last_click': usuario.last_click
-                }
-                usuarios_data.append(usuario_data)
-            return jsonify(usuarios_data)
-        except Exception as error:
-            print(error) #En caso de haber error lo imprimo
-            return jsonify({"Message": "No users found."}) # Acá queda a deber que tipo de error queremos proporcionarle
-        
+        return list_users()
     elif request.method == "POST":
-        try:
-            data = request.json
-            nuevo_nombre = data.get('name')         
-            # Se definen valores predeterminados si no se proporcionan
-            total_points = data.get('total_points', 0)
-            spent_points = data.get('spent_points', 0)
-            new_user = User(name=nuevo_nombre, total_points=total_points, spent_points=spent_points) #removido el last_click hasta resolver lo del horario
-            db.session.add(new_user)
-            db.session.commit()
+        return create_user()
+    else:
+        return jsnoify({"message": "Method not allowed."}), 405
 
-            new_upgrades = UserUpgrade(user_id=new_user.id)
-            db.session.add(new_upgrades)
-            db.session.commit()
+def list_users():
+    try:
+        usuarios = User.query.all()
+        usuarios_data = []
+        for usuario in usuarios:
+            usuario_data = {
+                'id': usuario.id,
+                'name': usuario.name,
+                'total_points': usuario.total_points,
+                'spent_points': usuario.spent_points,
+                'last_click': usuario.last_click
+            }
+            usuarios_data.append(usuario_data)
+        return jsonify(usuarios_data)
+    except Exception as error:
+        print(error) #En caso de haber error lo imprimo
+        return jsonify({"Message": "No users found."}), 500 
 
+def create_user():
+    try:
+        data = request.json
+        nuevo_nombre = data.get('name')         
+        # Se definen valores predeterminados si no se proporcionan
+        total_points = data.get('total_points', 0)
+        spent_points = data.get('spent_points', 0)
+        new_user = User(name=nuevo_nombre, total_points=total_points, spent_points=spent_points) #removido el last_click hasta resolver lo del horario
+        db.session.add(new_user)
+        db.session.commit()
 
-            return jsonify({'user': {'id': new_user.id, 'name': new_user.name, 'total_points': new_user.total_points, 'spent_points': new_user.spent_points, 
-                    'upgrades': {
+        new_upgrades = UserUpgrade(user_id=new_user.id)
+        db.session.add(new_upgrades)
+        db.session.commit()
+
+        return jsonify({
+            'user': {
+                'id': new_user.id, 
+                'name': new_user.name, 
+                'total_points': new_user.total_points, 
+                'spent_points': new_user.spent_points, 
+                'upgrades': {
                     'pickaxe': new_upgrades.pickaxe,
                     'lantern': new_upgrades.lantern,
                     'assistant': new_upgrades.assistant,
@@ -68,161 +78,172 @@ def all_users():
                     'housing': new_upgrades.housing,
                     'helmet': new_upgrades.helmet,
                     'cartographer': new_upgrades.cartographer
-                }}}), 201
-        except Exception as error:
+                }
+            }
+        }), 201
+    except Exception as error:
             print(error)
             return jsonify({"message": "Couldn't create user."}), 500 # Esto no deberia pasar nunca
 
-    else:
-        return jsonify({"message": "Method not allowed."}), 405
-        
+
 @app.route("/users/<user_id>/", methods=["GET", "PUT", "DELETE"])
 def user_by_id(user_id):
 
-    if request.method == "PUT":
-        try:
-            usuario = User.query.get(user_id)
-
-            if usuario:
-                data=request.json
-                if 'name' in data:
-                    usuario.name= data['name']
-                if 'total_points' in data:
-                    usuario.total_points = data['total_points']
-                if 'spent_points' in data:
-                    usuario.spent_points = data['spent_points']
-                db.session.commit()
-                return jsonify({"Mensaje": f"User with id:{user_id} updated"}), 200
-            
-            else:
-                return jsonify({"Mensaje": "User does not exist"}), 404
-            
-        except Exception as error:
-            print(f"The ${error} happened.")
-            return jsonify({"mensaje": f"Error when updating user {user_id}"}), 500
-
+    if request.method == "GET":
+        return get_user(user_id)
+    elif request.method == "PUT":
+        return update_user(user_id)
     elif request.method == "DELETE":
-        try:
-            # Busca al usuario por ID
-            user = User.query.get(user_id)
-
-            if user:
-                user_upgrade=UserUpgrade.query.filter_by(user_id=user.id).first()
-                if user_upgrade:
-                    db.session.delete(user_upgrade)               
-                db.session.delete(user)
-                db.session.commit()
-                return jsonify({"Message": "User succesfully eliminated."}), 200
-            
-            else:
-                return jsonify({"Message": "User not found."}), 404
-            
-        except Exception as error:
-            print(error) 
-            return jsonify({"message": "Error when deleting user."}), 400
-            
-    elif request.method == "GET":
-        try:
-            user = User.query.get(user_id)
-
-            if user:
-                user_data = {
-                    'id': user.id,
-                    'name': user.name,
-                    'total_points': user.total_points,
-                    'spent_points': user.spent_points,
-                    'last_click': user.last_click
-                }
-                return jsonify(user_data)
-            
-            else:
-                return jsonify({"mensaje": f"User {user_id} not found."}), 404
-            
-        except Exception as error:
-            print(error)
-            return jsonify({"message": f"Error when searching for user {user_id}."}), 500
-
+        return delete_user(user_id)
     else:
         return jsonify({"message": "Method not allowed."}), 405
+#Funcion para obtener user segun id
+def get_user(user_id):
+    try:
+        user = User.query.get(user_id)
+        if user:
+            user_data = {
+                'id': user.id,
+                'name': user.name,
+                'total_points': user.total_points,
+                'spent_points': user.spent_points,
+                'last_click': user.last_click
+            }
+            return jsonify(user_data)        
+        else:
+            return jsonify({"mensaje": f"User {user_id} not found."}), 404
+                    
+    except Exception as error:
+        print(error)
+        return jsonify({"message": f"Error when searching for user {user_id}."}), 500
+
+#Funcion para cambiar data del user segun id
+def update_user(user_id):               
+    try:
+        user = User.query.get(user_id)
+        if user:
+            data=request.json
+            if 'name' in data:
+                user.name= data['name']
+            if 'total_points' in data:
+                user.total_points = data['total_points']
+            if 'spent_points' in data:
+                user.spent_points = data['spent_points']
+            db.session.commit()
+            return jsonify({"Mensaje": f"User with id:{user_id} updated"}), 200
+        else:
+            return jsonify({"Mensaje": "User does not exist"}), 404
     
-    
+    except Exception as error:
+        print(error)
+        return jsonify({"mensaje": f"Error when updating user {user_id}"}), 500
+
+#Funcion borrar user segun id
+def delete_user(user_id):
+    try:
+        # Busca al usuario por ID
+        user = User.query.get(user_id)
+        if user:
+            user_upgrade=UserUpgrade.query.filter_by(user_id=user.id).first()
+            if user_upgrade:
+                db.session.delete(user_upgrade)               
+            db.session.delete(user)
+            db.session.commit()
+            return jsonify({"Message": "User succesfully eliminated."}), 200
+            
+        else:
+            return jsonify({"Message": "User not found."}), 404
+            
+    except Exception as error:
+        print(error) 
+        return jsonify({"message": "Error when deleting user."}), 400
+
+        
 @app.route("/users/<user_id>/upgrades/", methods=["GET", "POST", "PUT"])
 def get_upgrades_by_id(user_id):
     if request.method == 'GET':
-        try:
-            upgrades = UserUpgrade.query.filter_by(user_id=user_id).first()
-            if upgrades:
-                user_upgrades = {
-                    "user_id": upgrades.user_id,
-                    "pickaxe": upgrades.pickaxe,
-                    "lantern": upgrades.lantern,
-                    "assistant": upgrades.assistant,
-                    "meals": upgrades.meals,
-                    "housing": upgrades.housing,
-                    "helmet": upgrades.helmet,
-                    "cartographer": upgrades.cartographer,
-                }
-                return jsonify(user_upgrades)
-            else:
-                return jsonify({"message": f"User {user_id} not found."}), 404
-        except Exception as error:
-            print(error)
-            return jsonify({"message": f"Error when searching for user {user_id}."}), 500
-   
-    elif request.method=='PUT':
-        try: #ver si cambiar el user_id para evitar confusiones
-            upgrades=UserUpgrade.query.filter_by(user_id=user_id).first()
-            if not upgrades:
-                return jsonify({"message": f"User {user_id} not found."}), 404
-
-            data=request.json
-            upgrades.pickaxe=data.get('pickaxe', upgrades.pickaxe)
-            upgrades.lantern=data.get('lantern',upgrades.lantern)
-            upgrades.assistant=data.get('assistant',upgrades.assistant)
-            upgrades.meals=data.get('meals',upgrades.meals)
-            upgrades.housing=data.get('housing',upgrades.housing)
-            upgrades.helmet=data.get('helmet',upgrades.helmet)
-            upgrades.cartographer=data.get('cartographer',upgrades.cartographer)
-
-            db.session.commit()
-            return jsonify({"message": "Upgrades updated succesfully."}), 200
-                
-        except Exception as error:
-            print(error)
-            return jsonify({"message": "Couldn't update user upgrades."}), 500
-        
+        return list_user_upgrades(user_id)
+    elif request.method == 'PUT':
+        return update_user_upgrades(user_id)
     else:
         return jsonify({"message": "Method not allowed."}), 405
 
+def list_user_upgrades(user_id):
+    try:
+        upgrades = UserUpgrade.query.filter_by(user_id=user_id).first()
+        if upgrades:
+            user_upgrades = {
+                "user_id": upgrades.user_id,
+                "pickaxe": upgrades.pickaxe,
+                "lantern": upgrades.lantern,
+                "assistant": upgrades.assistant,
+                "meals": upgrades.meals,
+                "housing": upgrades.housing,
+                "helmet": upgrades.helmet,
+                "cartographer": upgrades.cartographer,
+            }
+            return jsonify(user_upgrades)
+        else:
+            return jsonify({"message": f"User {user_id} not found."}), 404
+    except Exception as error:
+        print(error)
+        return jsonify({"message": f"Error when searching for user {user_id}."}), 500
+   
+def update_user_upgrades(user_id):
+    try: #ver si cambiar el user_id para evitar confusiones
+        upgrades=UserUpgrade.query.filter_by(user_id=user_id).first()
+        if not upgrades:
+            return jsonify({"message": f"User {user_id} not found."}), 404
 
+        data=request.json
+        upgrades.pickaxe=data.get('pickaxe', upgrades.pickaxe)
+        upgrades.lantern=data.get('lantern',upgrades.lantern)
+        upgrades.assistant=data.get('assistant',upgrades.assistant)
+        upgrades.meals=data.get('meals',upgrades.meals)
+        upgrades.housing=data.get('housing',upgrades.housing)
+        upgrades.helmet=data.get('helmet',upgrades.helmet)
+        upgrades.cartographer=data.get('cartographer',upgrades.cartographer)
+
+        db.session.commit()
+        return jsonify({"message": "Upgrades updated succesfully."}), 200
+                
+    except Exception as error:
+        print(error)
+        return jsonify({"message": "Couldn't update user upgrades."}), 500
+
+        
 @app.route("/upgrades/", methods=["GET"])
 def get_all_upgrades():
-
     if request.method == "GET":
-        try:
-            upgrades = Upgrade.query.all()
-            upgrades_data = []
-            for upgrade in upgrades:
-                upgrade_data = {
-                    'id': upgrade.id,
-                    'internal_name': upgrade.internal_name,
-                    'effect': upgrade.effect,
-                    'value_tier_1': upgrade.value_tier_1,
-                    'name_tier_1': upgrade.name_tier_1,
-                    'value_tier_2': upgrade.value_tier_2,
-                    'name_tier_2': upgrade.name_tier_2,
-                    'value_tier_3': upgrade.value_tier_3,
-                    'name_tier_3': upgrade.name_tier_3,
-                    'value_tier_4': upgrade.value_tier_4,
-                    'name_tier_4': upgrade.name_tier_4,
-                    'value_tier_5': upgrade.value_tier_5,
-                    'name_tier_5': upgrade.name_tier_5
-                }
-                upgrades_data.append(upgrade_data)
-            return jsonify(upgrades_data)
-        except Exception as error:
-            print(error) #En caso de haber error lo imprimo
-            return jsonify({"Message": "No upgrades found."}) # Acá queda a deber que tipo de error queremos proporcionarle
+        return list_upgrades()
+    else:
+        return jsonify({"message": "Method not allowed."}), 405
+
+def list_upgrades():
+    try:
+        upgrades = Upgrade.query.all()
+        upgrades_data = []
+        for upgrade in upgrades:
+            upgrade_data = {
+                'id': upgrade.id,
+                'internal_name': upgrade.internal_name,
+                'effect': upgrade.effect,
+                'value_tier_1': upgrade.value_tier_1,
+                'name_tier_1': upgrade.name_tier_1,
+                'value_tier_2': upgrade.value_tier_2,
+                'name_tier_2': upgrade.name_tier_2,
+                'value_tier_3': upgrade.value_tier_3,
+                'name_tier_3': upgrade.name_tier_3,
+                'value_tier_4': upgrade.value_tier_4,
+                'name_tier_4': upgrade.name_tier_4,
+                'value_tier_5': upgrade.value_tier_5,
+                'name_tier_5': upgrade.name_tier_5
+            }
+            upgrades_data.append(upgrade_data)
+        return jsonify(upgrades_data)
+    except Exception as error:
+        print(error) #En caso de haber error lo imprimo
+        return jsonify({"Message": "No upgrades found."}), 500
 
 
 if __name__ == '__main__':
@@ -232,5 +253,4 @@ if __name__ == '__main__':
         db.create_all()
     app.run(host='0.0.0.0', debug=True, port=port)
     print('Server started!')
-
 
